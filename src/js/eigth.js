744579ca226c6dec47a08882953fe43c3934444b
BinaryBot0101 <rockganesha512@gmail.com>
2023-07-02 06:28:28 +0530

    changes

--- src/js/eigth.js
+++ src/js/eigth.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
    
     // Continue button click event handler
-    var continueBtn = document.getElementById('continue-btn6');
+    var continueBtn = document.getElementById('continue-btn');
     continueBtn.addEventListener('click', function() {
         // Navigate to the fourth page
         window.location.href = 'complete.html';
